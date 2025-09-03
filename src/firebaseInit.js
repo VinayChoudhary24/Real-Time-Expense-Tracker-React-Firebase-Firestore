0f39e537e12ca5950d00a24b5653590443ac8ef4
vinaychoudhary24 <vinaychoudhary994@gmail.com>
2025-09-03 13:07:01 +0530

    update firebase config

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,14 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAiss0wE4uUP7p-Kbom3tTjb-ke89tf2fE",
-  authDomain: "blog-app-ebc14.firebaseapp.com",
-  projectId: "blog-app-ebc14",
-  storageBucket: "blog-app-ebc14.firebasestorage.app",
-  messagingSenderId: "59986246421",
-  appId: "1:59986246421:web:31efeebd27f41daede071c",
+  // Your web app's Firebase configuration
 };
 
 const app = initializeApp(firebaseConfig);
